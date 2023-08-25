3fd331e77562cd44ee8da3ab40ea52e6ca598981
Shivam Sharma <shivamsharma2695@gmail.com>
2023-08-25 17:32:25 +0530

    adding supporting files

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, redirect, render_template
+
+app = Flask(__name__)
+@app.route("/")
+def Main():
+    return render_template('index.html')
+@app.route('/shivam')
+def shivam():
+    return "this is our shivam's Page"
+
+if __name__=='__main__':
+    app.run(debug=True)
+
